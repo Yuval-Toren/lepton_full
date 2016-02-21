f468a1f7940bfe5e7b39e28767d7043c6df424b0
YT <yt@psyphas.com>
2016-02-21 15:24:25 +0200

    added lep_i2c for organization

---
+++ SW4STM32/Psyphas/Debug/Application/User/lep_i2c.d
@@ -0,0 +1,1 @@
+Application/User/lep_i2c.o: ../Application/User/lep_i2c.c
--- SW4STM32/Psyphas/Debug/Application/User/subdir.mk
+++ SW4STM32/Psyphas/Debug/Application/User/subdir.mk
@@ -7,6 +7,7 @@ C_SRCS += \
 C:/Users/Yuval/workspace/Psyphas/Src/dma.c \
 C:/Users/Yuval/workspace/Psyphas/Src/gpio.c \
 C:/Users/Yuval/workspace/Psyphas/Src/i2c.c \
+../Application/User/lep_i2c.c \
 C:/Users/Yuval/workspace/Psyphas/Src/main.c \
 C:/Users/Yuval/workspace/Psyphas/Src/spi.c \
 C:/Users/Yuval/workspace/Psyphas/Src/stm32f4xx_hal_msp.c \
@@ -17,6 +18,7 @@ OBJS += \
 ./Application/User/dma.o \
 ./Application/User/gpio.o \
 ./Application/User/i2c.o \
+./Application/User/lep_i2c.o \
 ./Application/User/main.o \
 ./Application/User/spi.o \
 ./Application/User/stm32f4xx_hal_msp.o \
@@ -27,6 +29,7 @@ C_DEPS += \
 ./Application/User/dma.d \
 ./Application/User/gpio.d \
 ./Application/User/i2c.d \
+./Application/User/lep_i2c.d \
 ./Application/User/main.d \
 ./Application/User/spi.d \
 ./Application/User/stm32f4xx_hal_msp.d \
@@ -52,6 +55,14 @@ Application/User/gpio.o: C:/Users/Yuval/workspace/Psyphas/Src/gpio.c
 	@echo ' '
 
 Application/User/i2c.o: C:/Users/Yuval/workspace/Psyphas/Src/i2c.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F401xE -I"C:/Users/Yuval/workspace/Psyphas/Inc" -I"C:/Users/Yuval/workspace/Psyphas/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Users/Yuval/workspace/Psyphas/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Yuval/workspace/Psyphas/Drivers/CMSIS/Include" -I"C:/Users/Yuval/workspace/Psyphas/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/Yuval/workspace/Psyphas/Inc"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Application/User/%.o: ../Application/User/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo %cd%
